d293f22f814ecaf72cd0f0a359ff972ada3e4dfd
geraldwogan <geraldwogan@gmail.com>
2022-03-09 23:27:54 +0000

    Initial work with Selenium

--- info.md
+++ info.md
@@ -31,4 +31,27 @@ Let's try the above approach. (https://www.thepythoncode.com/article/reading-ema
 
 I had to use a device specific password to bypass 2-step authentication: https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4OfScAjeZuVa9NdZwliKl6so5suPRhRH72dewBEk7PflLBL4RRRIV2KQFXKZ2ZcE_LerGCrREx4mV0_NPplTdFJQvEoOQ
 
+## Form Completions
+
+Using Selenium for this? https://automatetheboringstuff.com/chapter11/#:~:text=Controlling%20the%20Browser%20with%20the%20selenium%20Module
+https://www.geeksforgeeks.org/automatically-filling-multiple-responses-into-a-google-form-with-selenium-and-python/
+
+{
+    venv\scripts\activate
+    pip install selenium
+    deactivate
+}
+
+Installed the chrome driver for Selenium to work with chrome: https://chromedriver.storage.googleapis.com/index.html?path=99.0.4844.51/
+1. Open webpage
+1. Click 'Email' button
+1. Fill-out form
+    1. Full Name
+    1. Your email
+    1. Your phone number (Optional)
+    1. Message
+    1. Click 'Send' button
+1. Close webpage
+1. Wait for 5 minutes (?)
+1. Check email for make sure we have a new Daft Enquiry for the property.
 
--- program.py
+++ program.py
@@ -5,6 +5,11 @@ import webbrowser
 import os
 import json
 import re
+from selenium import webdriver
+# from selenium import WebElement
+import time
+from selenium.webdriver.common.by import By
+
 
 # account credentials
 json_file = open("apartment-application-automation/secret.json")
@@ -13,60 +18,111 @@ json_file.close()
 password = variables["device_pass"]
 username = variables["username"]
 
-# Login to Gmail account using secrets.json variables
-imap = imaplib.IMAP4_SSL("imap.gmail.com",993)
-imap.login(username, password)
+def getPropertyLink():
 
-# Select folder to read from
-status, messages = imap.select("INBOX")
-# number of top emails to fetch
-N = 2
-# total number of emails
-messages = int(messages[0])
+    # Login to Gmail account using secrets.json variables
+    imap = imaplib.IMAP4_SSL("imap.gmail.com",993)
+    imap.login(username, password)
 
-for i in range(messages, messages-N, -1):
-    # fetch the email message by ID
-    res, msg = imap.fetch(str(i), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            # parse a bytes email into a message object
-            msg = email.message_from_bytes(response[1])
+    # Select folder to read from
+    status, messages = imap.select("INBOX")
+    # number of top emails to fetch
+    N = 2
+    # total number of emails
+    messages = int(messages[0])
 
-            # decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            print("Subject:", subject)
+    for i in range(messages, messages-N, -1):
+        # fetch the email message by ID
+        res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
 
-            # decode email sender
-            From, encoding = decode_header(msg.get("From"))[0]
-            if isinstance(From, bytes):
-                From = From.decode(encoding)
-            print("From:", From)     
+                # decode the email subject
+                subject, encoding = decode_header(msg["Subject"])[0]
+                print("Subject:", subject)
 
-            # Only proceed if it is a Property Alert from Daft.ie
-            if From == '"Daft.ie Property Alert" <noreply@daft.ie>':# & From == 'Daft.ie':
-                # if the email message is multipart
-                if msg.is_multipart():
-                    # iterate over email parts
-                    for part in msg.walk():
-                        # extract content type of email
-                        content_type = part.get_content_type()
-                        content_disposition = str(part.get("Content-Disposition"))
-                        try:
-                            # get the email body
-                            body = part.get_payload(decode=True).decode()
-                        except:
-                            pass
-                        if content_type == "text/plain" and "attachment" not in content_disposition:
-                            # print text/plain emails and skip attachments
-                            if('View Property' in body):
-                                # Find link using regex. The first link in the email will be for the property.
-                                links = re.findall('\((.*?)\)', body)
-                                # print(links)
-                                webbrowser.open(links[0])
+                # decode email sender
+                From, encoding = decode_header(msg.get("From"))[0]
+                if isinstance(From, bytes):
+                    From = From.decode(encoding)
+                print("From:", From)     
 
-                print("="*100)
+                # Only proceed if it is a Property Alert from Daft.ie
+                if From == '"Daft.ie Property Alert" <noreply@daft.ie>':# & From == 'Daft.ie':
+                    # if the email message is multipart
+                    if msg.is_multipart():
+                        # iterate over email parts
+                        for part in msg.walk():
+                            # extract content type of email
+                            content_type = part.get_content_type()
+                            content_disposition = str(part.get("Content-Disposition"))
+                            try:
+                                # get the email body
+                                body = part.get_payload(decode=True).decode()
+                            except:
+                                pass
+                            if content_type == "text/plain" and "attachment" not in content_disposition:
+                                # print text/plain emails and skip attachments
+                                if('View Property' in body):
+                                    # Find link using regex. The first link in the email will be for the property.
+                                    links = re.findall('\((.*?)\)', body)
 
-# close the connection and logout
-imap.close()
-imap.logout()
+                                    # Open link in web browser
+                                    # webbrowser.open(links[0])
+
+                    print("="*100)
+
+    # close the connection and logout
+    imap.close()
+    imap.logout()
+
+    return links[0]
+
+
+
+
+def formCompletion(link):
+
+
+
+    # wait for one second, until page gets fully loaded
+
+    data = ['Gerald Wogan', 'geraldwogan@gmail.com', '0879999999', 'Test message']
+
+
+    driver =  webdriver.Chrome('apartment-application-automation\chromedriver.exe')
+    time.sleep(5)
+
+    # Open webpage
+    webbrowser.open('https://www.daft.ie/for-rent/apartment-whitworth-road-drumcondra-dublin-3/3723347')
+
+    time.sleep(5)
+
+    e = driver.find_element(By.XPATH, '//button[text()="Email Agent"]')
+
+    # e = driver.find_element(By.CLASS_NAME("NewButton__StyledButton-yem86a-2 fhElqk"))
+    # email_button = driver.find_element_by_class_name(
+        # "NewButton__StyledButton-yem86a-2 fhElqk")
+
+    e.click()
+
+    # Use Selenium to fill out web form
+
+    # 1. Open webpage
+
+    # 1. Click 'Email' button
+    # 1. Fill-out form
+    #     1. Full Name
+    #     1. Your email
+    #     1. Your phone number (Optional)
+    #     1. Message
+    #     1. Click 'Send' button
+    # 1. Close webpage
+    # 1. Wait for 5 minutes (?)
+    # 1. Check email for make sure we have a new Daft Enquiry for the property.
+
+
+formCompletion('b')
 
